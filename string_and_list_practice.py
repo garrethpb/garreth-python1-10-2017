f25eff1c6ee0767354a97065148f60c5da349c50
Garreth Broesche <gbroesche@gmail.com>
2017-10-03 08:49:28 -0500

    first commit for python repo

---
+++ string_and_list_practice.py
@@ -0,0 +1,26 @@
+# find
+string = "I like chocolate."
+print string.find("chocolate")
+print string
+
+# replace
+string = "I like eggplant"
+string = string.replace("eggplant", "chocolate")
+print string
+
+# min and max
+list = [-1, 999, 7.8, -32]
+print list
+print max(list)
+print min(list)
+
+# first, last, and len
+list = ["Garreth", 22, 47, "Yes", "Paul"]
+print len(list)
+print list[0], list[len(list) - 1]
+
+# sort
+list = [5, 'x', 'a', 1, 'm'];
+list.sort();
+print list
+
